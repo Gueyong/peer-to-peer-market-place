8a4928fa4dc27da9ae10e77fcf240a82b3a93e4a
favour <christdivinefaeur@gmail.com>
2024-07-02 07:35:48 +0100

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI kits",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: "editor picks",
+                href: '#',
+                imageSrc: 'nav/ui-kits/mixed'
+            }
+        ]
+    }
+
+]
